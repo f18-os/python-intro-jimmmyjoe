ab83f721ebc3cfb8cb96bbb8c4c420367d225482
jimmmyjoe <jimmyjoe@gmx.com>
2018-09-17 15:35:48 -0600

    deciding what structure to use

--- shell/shel.py
+++ shell/shel.py
@@ -1,16 +1,50 @@
+# Named after Shel Silverstein
+
 import sys, os, time, re
 from threading import Thread, enumerate
 
 class Worker(Thread):
     def __init__(self, cmd):
         self.args = re.split(' ', cmd)
+        self.paths = re.split(':', os.environ['PATH'])
         Thread.__init__(self)
 
     def run(self):
         print(self.name + ' is starting.')
-        
+
+def parse(command):
+
+    if not command:
+        sys.exit(1)
+
+    args = re.split(' ', command)
+    paths = re.split(':', os.environ['PATH'])
+
+    # Check for input (<) and output (>) redirect.
+    
+    # Might need to be a simple condition check
+    # because < and > simply modify file descriptors
+    # and don't need to know about past/future commands.
+
+    for arg in args:
+        if(arg === '<'):
+            #
+        elif(arg === '>'):
+            #
+
+    # Adjust file descriptors accordingly
+
+    # Implement pipe.
+
+    # Look ahead to find pipe arguments, example:
+    # apt-cache search regex | grep c++ > regexforcpp.txt
+
+    # <command>
+    #     |
+    # <program> <arg0|arg1|...> {<i/o_redirect> <file_path>} {<pipe> <command>}
 
 def main():
+    
     flag = True
     while(flag):
         cmd = input('>> ')
@@ -18,7 +52,7 @@ def main():
         if cmd == 'quit':
             sys.exit(0)
             
-        rc = os.fork()
+        rc = os.fork() #!!!
 
         if rc < 0:
             print('Fork failure.')
@@ -27,7 +61,7 @@ def main():
         elif rc == 0:
             args = re.split(' ', cmd)
             paths = re.split(':', os.environ['PATH'])
-            
+
             for dir in paths:
                 fullPath = '%s/%s' % (dir, args[0])
                 try:
